8bfaeb12cc297ee9e2a0f884297c9337193ba323
Diego Moreira Sardinha de Souza <diegoaguia31@gmail.com>
2025-07-23 09:49:03 +0100

    (update) test player ok

--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -1,5 +1,6 @@
 import type { Route } from "./+types/home";
 import coldplay from '../../assets/images/album.jpg'
+import MediaThemeSutroAudio from 'player.style/sutro-audio/react';
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -24,21 +25,17 @@ export default function Home() {
     <source src="/assets/videos/testesite.mp4" type="video/mp4" />
   </video>
 
-  <div className="relative z-10 flex items-center justify-center w-screen h-screen bg-slate-900/30 backdrop-blur-[1px]">
-    <div className="flex flex-col items-center justify-center w-4/12 h-3/6 bg-sky-400/70 rounded-lg shadow-lg ">
-        <div      
-        style={{ backgroundImage: `url(${coldplay})`,
-          backgroundSize: 'contain',
-          backgroundRepeat: 'no-repeat',
-          backgroundPosition: 'center',
-         }}
-      className="h-48 w-48 rounded-md shadow-md">
-        </div>
-        <div>
-            <h1 className="mt-2 text-2xl text-shado-sm">Coldplay - Paradaise</h1>
-        </div>
-        
-    </div>
+  <div className="relative z-10 flex  items-end w-screen h-screen bg-slate-900/30 backdrop-blur-[1px]">
+  <div className="flex flex-col w-full " >
+         <MediaThemeSutroAudio  >
+        <audio
+          slot="media"
+          src="http://localhost:3000/media/cmdd9vmhv00034y0w26gmdo5d"
+          playsInline
+          crossOrigin="anonymous"
+        ></audio>
+      </MediaThemeSutroAudio>
+ </div>
     
   </div>
 </section>

c4920200e34e3a30c6516058473f6cf123e31335
Diego Moreira Sardinha de Souza <diegoaguia31@gmail.com>
2025-08-22 11:17:16 +0100

    (update) start create login page

--- frontend/app/routes/login.tsx
+++ frontend/app/routes/login.tsx
@@ -1,29 +1,73 @@
-// import { useNavigate } from "react-router-dom";
-import { Descope } from "@descope/react-sdk";
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 export default function Login() {
-//   const navigate = useNavigate();
-
   return (
-    <div className="root">
-      <div>
-        <h1>Log in With Descope</h1>
-        <div className="btn-container">
-          <Descope
-            flowId="sign-up-or-in"
-            theme="light"
-            onSuccess={(e) => {
-              console.log(e.detail.user.name);
-              console.log(e.detail.user.email);
-              navigate("/secure");
-            }}
-            onError={(err) => {
-              console.log("Error!", err);
-              alert("Error: " + err.detail.message);
-            }}
-          />
-        </div>
-      </div>
+    <div className="flex items-center justify-center w-screen h-screen bg-[#212124]">
+          <Tabs defaultValue="SingIn" className="cursor-pointer w-full max-w-sm dark">
+            <TabsList className="cursor-pointer">
+              <TabsTrigger value="SingIn" className="cursor-pointer">SingIn</TabsTrigger>
+              <TabsTrigger value="Singup" className="cursor-pointer">Singup</TabsTrigger>
+            </TabsList>
+        <TabsContent value="SingIn">
+              <Card className="w-full max-w-sm dark">
+                <CardHeader>
+                  <img src='../../assets/images/logo.png' alt="Logo" className="w-16 h-16 mx-auto mb-4" />
+                  <CardTitle className="text-center">Login to your account</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <form>
+                    <div className="flex flex-col gap-6">
+                      <div className="grid gap-2">
+                        <Label htmlFor="email">Email</Label>
+                        <Input
+                          id="email"
+                          type="email"
+                          placeholder="your_email@example.com"
+                          required
+                        />
+                      </div>
+                      <div className="grid gap-2">
+                        <div className="flex items-center">
+                          <Label htmlFor="password">Password</Label>
+                          <a
+                            href="#"
+                            className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                          >
+                            Forgot your password?
+                          </a>
+                        </div>
+                        <Input id="password" type="password" required />
+                      </div>
+                    </div>
+                  </form>
+                </CardContent>
+                <CardFooter className="flex-col gap-2">
+                  <Button type="submit" className="w-full cursor-pointer">
+                    Login
+                  </Button>
+                  <Button variant="outline" className="w-full cursor-pointer">
+                    <img src='../../assets/images/google.png' alt="Google Icon" className="w-5 h-5 mr-2" />
+                    Login with Google
+                  </Button>
+                </CardFooter>
+              </Card>
+          <TabsContent value="SingUp">
+
+          </TabsContent>
+        </TabsContent>
+          </Tabs>
     </div>
-  );
+  )
 }
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,20 @@
-import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { reactRouter } from "@react-router/dev/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths()
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./app"),
+    },
+  },
 });
